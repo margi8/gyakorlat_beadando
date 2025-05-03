6692cf99bd24f70688982b119d65840488f31679
margi8 <margi8@hallgato.nje.hu>
2025-05-03 23:13:23 +0200

    version3

---
+++ templates/pages/fooldal.tpl.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Okos Otthon Tervezés</title>
+</head>
+<body>
+
+<div class="container">
+<h1>Fedezze fel az okos otthon tervezés jövőjét!</h1>
+<p>
+    Az okos otthonok forradalmasítják életünket azáltal, hogy kényelmesebbé, biztonságosabbá és energiatakarékosabbá teszik otthonainkat. 
+    A mesterséges intelligencia integrálása lehetővé teszi, hogy otthonunk előre megértse és kiszolgálja igényeinket – automatikusan szabályozza a fűtést, világítást, biztonsági rendszereket és még sok mást.
+</p>
+
+<video src="../../videos/intro.mp4" class="media-right" controls autoplay muted></video>
+
+<p>
+    Egy jól megtervezett okos otthon figyelembe veszi a lakók napi rutinját, és ahhoz igazítja működését. A redőnyök reggel automatikusan felgördülnek, a kávéfőző elindul, és a világítás fokozatosan világosodik. 
+    Mindez csökkenti az energiafogyasztást, miközben kényelmesebb életet biztosít. A rendszer figyeli a környezeti változásokat is, például az időjárást, és ehhez igazítja a működést.
+</p>
+
+<div class="clear"></div>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/hIEIGDsbKqY?si=6Q2hp4QsaLI3_BWp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+<p>
+    A mesterséges intelligencia nemcsak kényelmet nyújt, hanem fokozza a biztonságot is. 
+    Az intelligens kamerarendszerek felismerik a mozgást és személyeket, riasztanak, ha gyanús tevékenységet észlelnek. 
+    A bejárati ajtó okos zárral működik, amely távolról is vezérelhető, vagy automatikusan záródik, ha senki nincs otthon. 
+    Az AI a hangalapú asszisztensek révén lehetővé teszi, hogy szóban vezéreljük a háztartás szinte bármely funkcióját.
+</p>
+
+<p>
+    A tervezés során fontos figyelembe venni a lakás felépítését, a szükséges eszközöket és azok kompatibilitását. 
+    Egy megbízható rendszer rugalmasan bővíthető, így később is integrálhatók új funkciók. 
+    Az adatvédelem is kiemelt szerepet kap, hiszen a rendszer sok személyes adatot kezel – ezeket biztonságosan kell tárolni és feldolgozni.
+</p>
+
+<div class="clear"></div>
+
+<h2>Irodánk helye a térképen</h2>
+<iframe 
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2689.497637956753!2d19.040235315635157!3d47.497912479177424!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741dc1757a2f6b9%3A0x6f9541b0dc75a5f7!2sBudapest!5e0!3m2!1shu!2shu!4v1581680761579" 
+    width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="">
+</iframe>
+</div>
+</body>
+</html>
